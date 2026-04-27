9fcad42a4864d0de47c34269b885bc5d441097c5
mrSquidee <mr.squidy78@gmail.com>
2026-04-27 17:02:44 -0400

    main thread pseudocode

---
+++ trampoline-fighter/synchronization_handler.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+class_name SynchronizationHandler
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	#Get local input
+	#Get remote input
+	#Figure out conflicts
+	#Start threads at correct frame
+	
+	pass
